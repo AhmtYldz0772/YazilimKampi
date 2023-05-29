aa448c6cf45ee13283a4af1fffb517bcc37da505
Ahmet <7207.yildiz@gmail.com>
2023-05-29 20:43:31 +0300

    Create AsalSayi.java

---
+++ Hafta_1/src/AsalSayi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class AsalSayi {
+    public static void main(String[] args) {
+        System.out.println("1 ile 100 arasindaki asal sayilar:");
+
+        for (int i = 2; i <= 100; i++)
+        {
+            boolean asalMi = true;
+
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            if (asalMi)
+            {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
