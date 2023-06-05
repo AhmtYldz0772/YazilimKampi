fa07b593e20b37bc8f6bc570b5070dd79fb74f65
Ahmet <7207.yildiz@gmail.com>
2023-06-05 12:19:10 +0300

    Update palindrom.java

--- Hafta_2/palindrom.java
+++ Hafta_2/palindrom.java
@@ -1,31 +1,29 @@
-import java.util.Scanner;
-public class palindrom {
-    static  boolean isPalidrom(int number)
-    {
-        int temp = number, reversNumber=0, lastNumber;
-        while(temp != 0)
-        {
-            System.out.println("Sayi:" + temp);
-            lastNumber = temp % 10;
-            System.out.println("Son basamak:" + lastNumber);
-            reversNumber = (reversNumber * 10) + lastNumber;
-            System.out.println("yeni sayı:" + reversNumber);
-            temp /= 10;
+public class Palindrom {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
         }
-        if(number == reversNumber)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return true;
     }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("bir sayı gir");
-        int x = input.nextInt();
-        System.out.println(isPalidrom(x));
 
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
     }
 }
+
