306ec81003ca011cbf18dac4c9bc8978b56b7343
Ahmet <7207.yildiz@gmail.com>
2023-06-06 12:55:35 +0300

    mayin tarlasi

---
+++ Hafta_2/MayinTarlasi.java
@@ -0,0 +1,11 @@
+
+import java.util.Random;
+import java.util.Scanner;
+public class MayinTarlasi {
+    public static void main(String[] args) {
+
+
+        MineSwepper mineSwepper = new MineSwepper(4,4);
+
+    }
+}
